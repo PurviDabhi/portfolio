0812550a3ac2c96624c0ca8b98be63dbeda81f2b
PurviDabhi <purvidabhi73@gmail.com>
2023-12-29 08:59:49 +0530

    title fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Navbar from './Components/Navbar';
+
 import Home from './Components/Home';
 import About from './Components/About';
 import Skills from './Components/Skills';
