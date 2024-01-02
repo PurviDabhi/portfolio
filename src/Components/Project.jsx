2c1df86e33a65a2dfa15446514db6c9ad37128d2
PurviDabhi <purvidabhi73@gmail.com>
2024-01-02 12:38:34 +0530

    projectUpdated

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -6,6 +6,9 @@ import { LuListTodo } from "react-icons/lu";
 import { ImSpoonKnife } from "react-icons/im";
 
 export default function Project() {
+
+
+
     return (
         <>
             <div className="min-h-screen" id="project">
@@ -22,13 +25,16 @@ export default function Project() {
                             <h1 className="text-3xl font-semibold ">HeadlineHub</h1>
                             <p className="text-sm">The primary objective of the Newsapp is to offer users a convenient platform for accessing diverse and up-to-date news content from various reliable sources. The app aims to deliver an intuitive and engaging interface, catering to the preferences and interests of a wide range of users</p>
                         </div>
-                        <div className="border rounded-xl shadow-md shadow-slate-300 flex flex-col gap-3 items-center justify-center px-3 py-2 hover:bg-blue-500 hover:text-white transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-95" data-aos="fade-up">
-                            <div className="rounded-full text-white px-3 py-3 bg-blue-500">
-                                <TiWeatherPartlySunny size={30} />
+                        <a href="https://weatherapp-eta-lac.vercel.app/">
+                            <div className="border rounded-xl shadow-md shadow-slate-300 flex flex-col gap-3 items-center justify-center px-3 py-2 hover:bg-blue-500 hover:text-white transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-95" data-aos="fade-up">
+                                <div className="rounded-full text-white px-3 py-3 bg-blue-500">
+                                    <TiWeatherPartlySunny size={30} />
+                                </div>
+                                <h1 className="text-3xl font-semibold ">Climate Craze</h1>
+                                <p className="text-sm ">The Weather App is developed to offer users a straightforward and informative interface for accessing accurate weather forecasts based on their location or specified locations. Utilizing modern web technologies, the app delivers a seamless user experience and provides valuable meteorological data from trusted sources</p>
                             </div>
-                            <h1 className="text-3xl font-semibold ">Climate Craze</h1>
-                            <p className="text-sm ">The Weather App is developed to offer users a straightforward and informative interface for accessing accurate weather forecasts based on their location or specified locations. Utilizing modern web technologies, the app delivers a seamless user experience and provides valuable meteorological data from trusted sources</p>
-                        </div>
+                        </a>
+
                         <div className="border rounded-xl shadow-md shadow-slate-300 flex flex-col gap-3 items-center justify-center px-3 py-2 hover:bg-blue-500 hover:text-white transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-95  " data-aos="fade-up">
                             <div className="rounded-full text-white px-3 py-3 bg-blue-500">
                                 <RiMovie2Fill size={30} />
@@ -38,13 +44,16 @@ export default function Project() {
                         </div>
                     </div>
                     <div className="grid md:grid-cols-3 gap-4 px-4 grid-cols-1 md:mt-6 mt-3 md:mb-5">
-                        <div className="border rounded-xl shadow-md shadow-slate-300 flex flex-col gap-3 items-center justify-center px-3 py-2 hover:bg-blue-500 hover:text-white transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-95  " data-aos="fade-up">
-                            <div className="rounded-full text-white px-3 py-3 bg-blue-500">
-                                <FaQuoteRight size={30} />
+                        <a href="https://purvidabhi.github.io/quotegenerator/">
+                            <div className="border rounded-xl shadow-md shadow-slate-300 flex flex-col gap-3 items-center justify-center px-3 py-2 hover:bg-blue-500 hover:text-white transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-95  " data-aos="fade-up">
+                                <div className="rounded-full text-white px-3 py-3 bg-blue-500">
+                                    <FaQuoteRight size={30} />
+                                </div>
+                                <h1 className="text-3xl font-semibold ">Quote Quest</h1>
+                                <p className="text-sm ">A quote generator is a software tool or application designed to display random or curated quotations to users. It serves as a source of inspiration, motivation, reflection, or entertainment by presenting diverse quotes from various categories, including literature, famous personalities, movies, or speeches</p>
                             </div>
-                            <h1 className="text-3xl font-semibold ">Quote Quest</h1>
-                            <p className="text-sm ">A quote generator is a software tool or application designed to display random or curated quotations to users. It serves as a source of inspiration, motivation, reflection, or entertainment by presenting diverse quotes from various categories, including literature, famous personalities, movies, or speeches</p>
-                        </div>
+                        </a>
+                        
                         <div className="border rounded-xl shadow-md shadow-slate-300 flex flex-col gap-3 items-center justify-center px-3 py-2 hover:bg-blue-500 hover:text-white transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-95  " data-aos="fade-up">
                             <div className="rounded-full text-white px-3 py-3 bg-blue-500">
                                 <LuListTodo size={30} />
